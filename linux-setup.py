ac9b4115b33712fc84c510c2046fde527f4998fe
DOKKIITECH <168053865+dokkiitech@users.noreply.github.com>
2025-03-11 22:16:22 +0900

    Update linux-setup.py

--- linux-setup.py
+++ linux-setup.py
@@ -1,39 +1,35 @@
-import os
 import subprocess
 
 def run_command(command):
-    print(f"Executing: {command}")
-    process = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-    if process.stdout:
-        print(process.stdout)
-    if process.stderr:
-        print(process.stderr)
+    """指定したコマンドを実行し、出力を表示する"""
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout.decode())
+    if stderr:
+        print(stderr.decode())
 
-def install_dependencies():
-    print("Installing required dependencies...")
+def setup_node():
+    print("[1/5] システムをアップデート中...")
     run_command("sudo yum update -y")
-    run_command("sudo yum groupinstall -y 'Development Tools'")
+    
+    print("[2/5] 必要なパッケージをインストール中...")
     run_command("sudo yum install -y curl tar git gcc gcc-c++ make")
-
-def install_nvm():
-    print("Installing NVM (Node Version Manager)...")
+    
+    print("[3/5] Node.js (LTS) をインストール中...")
     run_command("curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash")
     run_command("export NVM_DIR=\"$HOME/.nvm\" && [ -s \"$NVM_DIR/nvm.sh\" ] && \\ . \"$NVM_DIR/nvm.sh\" && nvm install --lts")
-
-def configure_node():
-    print("Setting up Node.js environment...")
     run_command("export NVM_DIR=\"$HOME/.nvm\" && [ -s \"$NVM_DIR/nvm.sh\" ] && \\ . \"$NVM_DIR/nvm.sh\" && nvm use --lts")
-    run_command("node -v")
+    
+    print("[4/5] npm のバージョン確認...")
     run_command("npm -v")
-    run_command("npm install -g npm")
-    run_command("npm install -g yarn")
+    
+    print("[5/5] pm2 をインストール（オプション）...")
     run_command("npm install -g pm2")
-
-def main():
-    install_dependencies()
-    install_nvm()
-    configure_node()
-    print("Node.js setup is complete!")
+    
+    print("Node.js のバージョンを確認...")
+    run_command("node -v")
+    
+    print("✅ Node.js 環境のセットアップが完了しました！")
 
 if __name__ == "__main__":
-    main()
+    setup_node()
